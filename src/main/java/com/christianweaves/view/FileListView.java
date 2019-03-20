4219705808e8b335e87ff94035476fd600fcbc06
csw <christianweaves@gmail.com>
2019-03-20 23:33:49 +0000

    CWDOTCOM-36 extract strings to message bundle

--- src/main/java/com/christianweaves/view/FileListView.java
+++ src/main/java/com/christianweaves/view/FileListView.java
@@ -1,32 +1,23 @@
 package com.christianweaves.view;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.List;
+import java.util.ResourceBundle;
 
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
-import org.apache.commons.io.FileUtils;
-import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.UploadedFile;
 
 @Named
 @RequestScoped
 public class FileListView {
  
-	private String fileLocation = "/home/blog/uploads";
+	ResourceBundle bundle = ResourceBundle.getBundle("Messages");
+	
+	private String fileLocation = bundle.getString("uploadResourceFolder");
 	
 	private Collection<File> files = new ArrayList<>();
 
