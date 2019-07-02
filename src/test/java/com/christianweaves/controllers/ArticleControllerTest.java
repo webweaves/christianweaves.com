177baf2b5c10cdd83c9c884fb4622654eb8c81f6
csw <christianweaves@gmail.com>
2019-07-03 00:04:24 +0100

    Continuing with unit tests

--- src/test/java/com/christianweaves/controllers/ArticleControllerTest.java
+++ src/test/java/com/christianweaves/controllers/ArticleControllerTest.java
@@ -1,35 +1,45 @@
 package com.christianweaves.controllers;
 
+
+import com.christianweaves.entities.Article;
+import com.christianweaves.entities.ArticleDao;
+
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 
-import com.christianweaves.entities.Article;
-import com.christianweaves.entities.ArticleDao;
 
 public class ArticleControllerTest {
 
 	
-	@Mock 
+	
 	private ArticleDao articleDao;
+	
+	@InjectMocks ArticleController articleController;
 	
 	@Before
 	public void init() {
 		articleDao = Mockito.mock(ArticleDao.class);
+
+		doReturn(new Article()).when(articleDao).getArticleById(1L);
+		
 		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
 	public void testShowFilteredArticle() {
-		ArticleController articleController = new ArticleController();
 		
-		when(articleController.showArticle(1L)).thenReturn(new Article());
 		
 		Article article = articleController.showFilteredArticle(1L);
+		
+		int i =0;
+		i++;
 	}
 }
