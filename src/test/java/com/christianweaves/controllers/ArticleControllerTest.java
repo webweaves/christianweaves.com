90dedb80eb2e4569f9a2cd3f954b6e840e0c1273
csw <christianweaves@gmail.com>
2019-07-15 23:56:33 +0100

    Tests

--- src/test/java/com/christianweaves/controllers/ArticleControllerTest.java
+++ src/test/java/com/christianweaves/controllers/ArticleControllerTest.java
@@ -9,8 +9,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.when;
-import static org.junit.Assert.assertEquals;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.doReturn;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,19 +29,32 @@ public class ArticleControllerTest {
 	
 	private ArticleDao articleDao;
 	
+	@Mock ExternalContext externalContext;
+	@Mock HttpServletRequest request;
+	
 	@InjectMocks ArticleController articleController;
 	
+	FacesContext facesContext;
+	
 	@Before
-	public void init() {
+	public void initMocks() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		facesContext = ContextMocker.mockFacesContext();		
 		dummyArticle.setBody("<p>&nbsp;</p>This is a test");
+
 		articleDao = Mockito.mock(ArticleDao.class);
 		doReturn(dummyArticle).when(articleDao).getArticleById(1L);
-		MockitoAnnotations.initMocks(this);
+		
 	}
 	
 	@Test
 	public void testShowFilteredArticle() {
 		Article article = articleController.showFilteredArticle(1L);
 		assertEquals("This is a test", article.getBody());
+	}
+	
+	@Test public void testSave() {
+		articleController.save();
+		verify(articleController).save();
 	}
 }
