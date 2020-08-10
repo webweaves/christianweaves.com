3ca6c11bf130d357d8307b81007467d4619769fb
csw <christianweaves@gmail.com>
2020-08-10 23:58:01 +0100

    CWDOTCOM-17 Refactored search components

--- src/main/java/com/christianweaves/lucene/InMemorySearch.java
+++ src/main/java/com/christianweaves/lucene/InMemorySearch.java
@@ -1,7 +1,6 @@
 package com.christianweaves.lucene;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +8,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -20,8 +17,6 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
@@ -35,8 +30,10 @@ import com.christianweaves.entities.ArticleDao;
 
 @Named
 @RequestScoped
-public class Indexer {
+public class InMemorySearch {
 
+	private int resultCount = 10;
+	
 	@Inject
 	private ArticleDao dao;
 	
@@ -57,7 +54,7 @@ public class Indexer {
 		                             + " " + article.getBody();
 		        
 		        doc.add(new StringField("articleId", id, Field.Store.YES));		
-		        doc.add(new TextField("description", description, Field.Store.YES));		
+		        doc.add(new TextField("description", description.toLowerCase(), Field.Store.YES));		
 				writer.addDocument(doc);
 			}
 	
@@ -66,15 +63,14 @@ public class Indexer {
 			e.printStackTrace();
 		}
 		List<String> r = new ArrayList<>();
-		int n = 5;
 		
 		try {
 			//create a term to search file name
-		    Term term = new Term("description", getQuery());
+		    Term term = new Term("description", getQuery().toLowerCase());
 		    //create the term query object
 		    Query query = new FuzzyQuery(term, 2);
 		    IndexSearcher indexSearcher = new IndexSearcher(DirectoryReader.open(dir));
-		    TopDocs topDocs = indexSearcher.search(query, n);
+		    TopDocs topDocs = indexSearcher.search(query, resultCount);
 			for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
 		        r.add(indexSearcher.doc(scoreDoc.doc).get("description"));
 		    }
@@ -85,7 +81,7 @@ public class Indexer {
 			for (int i=0; i<hits.length; ++i) {
 			    int docId = hits[i].doc;
 			    Document d = indexSearcher.doc(docId);
-			    results.add(dao.getArticleById(Long.getLong(d.get("articleId"))));
+			    results.add(dao.getArticleById(Long.valueOf(d.get("articleId"))));
 			}
 			
 		} catch (IOException e) {
@@ -94,7 +90,7 @@ public class Indexer {
 	}
 	
 	public static void main(String[] args) {
-		new Indexer().createIndex();
+		new InMemorySearch().createIndex();
 		
 	}
 
